ada83e04e55cba3a55d173185be2a6cf65130d85
w2ppx <w2ppx@users.noreply.github.com>
2026-05-27 17:18:36 +0300

    fix: parse reality fingerprint from JSON and YAML subscriptions

--- app/src/main/java/io/nekohasekai/sagernet/group/V2RayJSONParser.kt
+++ app/src/main/java/io/nekohasekai/sagernet/group/V2RayJSONParser.kt
@@ -196,6 +196,9 @@ fun parseV2RayOutbound(outbound: JsonObject): List<AbstractBean> {
                                 realitySettings.getString("mldsa65Verify")?.also {
                                     v2rayBean.realityMldsa65Verify = it
                                 }
+                                realitySettings.getString("fingerprint")?.also {
+                                    v2rayBean.realityFingerprint = it
+                                }
                             }
                         }
                     }
